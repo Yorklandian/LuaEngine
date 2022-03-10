9a857bad7dea32b9359e13d013adad819df444d7
YK <1627936869@qq.com>
2022-03-10 17:28:15 +0800

    已实现多对象异步

--- Scripts/GameObject.lua
+++ Scripts/GameObject.lua
@@ -10,6 +10,7 @@ local GameObject={
     top=0,
     width=0,
     height=0,
+    rotation=0,
 }
 GameObject.__index=GameObject
 
@@ -21,21 +22,57 @@ function GameObject:Create(id,path,left,top,width,height)
     g.top=top or 0
     g.height=height or 0
     g.width=width or 0
+    g.rotation=0
     g.img=IMGLib.loadImage(path,width,height)
-    print("img load")
     IMGLib.putImage(g.img,left,top)
 
     return g
 end
 
 function GameObject:Move(steps)
+    local deltaX=math.sin(self.rotation)
+    local deltaY=math.cos(self.rotation)
     for i=1,steps do
         IMGLib.clearRectangle(self.left,self.top,self.left+self.width,self.top+self.height)
-        self.top=self.top+10
+        self.left=self.left-deltaX*10
+        self.top=self.top-deltaY*10
         IMGLib.putImage(self.img,self.left,self.top)
         wiat(0.5)
     end
-    
+end
+
+function GameObject:Turn(Radian)
+    self.rotation=self.rotation-Radian
+    self.rotation=self.rotation%(math.pi*2)
+    IMGLib.rotateImage(self.img,-Radian)
+    IMGLib.clearRectangle(self.left,self.top,self.left+self.width,self.top+self.height)
+    IMGLib.putImage(self.img,self.left,self.top)
+end
+
+function GameObject:SlideTo(x,y,speed)
+    local deltaX=x-self.left
+    local deltaY=y-self.top
+
+    deltaX=deltaX*speed
+    deltaY=deltaY*speed
+    if x>=self.left then
+        while x>self.left do
+            IMGLib.clearRectangle(self.left,self.top,self.left+self.width,self.top+self.height)
+            self.left=self.left+deltaX
+            self.top=self.top+deltaY
+            IMGLib.putImage(self.img,self.left,self.top)
+            wiat(0.03)
+        end
+    else
+        while x<self.left do
+            IMGLib.clearRectangle(self.left,self.top,self.left+self.width,self.top+self.height)
+            self.left=self.left+deltaX
+            self.top=self.top+deltaY
+            IMGLib.putImage(self.img,self.left,self.top)
+            wiat(0.03)
+        end
+    end
+
 end
 
 return GameObject
--- Scripts/test.lua
+++ Scripts/test.lua
@@ -11,17 +11,17 @@ local wait=TaskManager.wait
 
 local g1=GameObject:Create(1,"./Resource/Smile.jpg",200,200,50,50)
 local g2=GameObject:Create(2,"./Resource/Smile2.jpg",500,500,50,50)
-print(g1.left)
-print(g2.left)
 
 function task1()
     g1:Move(2)
     wait(0.1)
-    g1:Move(5)
+    g1:Turn(math.pi/3)
+    g1:SlideTo(500,600,0.005)
 end
 
 function task2()
     g2:Move(5)
+    g2:SlideTo(0,0,0.01)
 end
 
 tm.launch(task1,"task1")
